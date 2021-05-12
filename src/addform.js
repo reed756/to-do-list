ff155b28a542d97b5faacee420ba0e6e783e978f
reed756 <68922323+reed756@users.noreply.github.com>
2021-05-12 18:48:12 +0100

    added event listeners to open and close add todo form

--- src/addform.js
+++ src/addform.js
@@ -1,15 +1,49 @@
 const addForm = () => {
-    let normal = document.querySelector(".default");
+    let normal = document.querySelector(".default2");
     let form = document.createElement('form');
+    let br1 = document.createElement('br');
+    let br2 = document.createElement('br');
+    let br3 = document.createElement('br');
+    let br4 = document.createElement('br');
+
+    let labelTitle = document.createElement('label');
     let inputTitle = document.createElement('input');
+    labelTitle.textContent = "Title:";
+
+    let labelDescription = document.createElement('label');
     let inputDescription = document.createElement('input');
+    labelDescription.textContent = "Description:";
+
+    let labelDueDate = document.createElement('label');
     let inputDueDate = document.createElement('input');
+    labelDueDate.textContent = "Due Date:";
+
+    let labelPriority = document.createElement('label');
     let inputPriority = document.createElement('input');
+    labelPriority.textContent = "Priority:";
+
+    let button = document.createElement('button');
+    let cancel = document.createElement('button');
+    button.textContent = "ADD TODO";
+    button.setAttribute('type', 'button');
+    cancel.textContent = "CANCEL";
+    cancel.classList.add('cancel');
+    cancel.setAttribute('type', 'button');
     
+    form.appendChild(labelTitle);
     form.appendChild(inputTitle);
+    form.appendChild(br1);
+    form.appendChild(labelDescription);
     form.appendChild(inputDescription);
+    form.appendChild(br2);
+    form.appendChild(labelDueDate);
     form.appendChild(inputDueDate);
+    form.appendChild(br3);
+    form.appendChild(labelPriority);
     form.appendChild(inputPriority);
+    form.appendChild(br4);
+    form.appendChild(button);
+    form.appendChild(cancel);
 
     normal.appendChild(form);
 }
--- src/index.js
+++ src/index.js
@@ -17,6 +17,18 @@ let newTodo = createTodo('title', 'description', 'dueDate', 'priority3');
 let wewTodo = createTodo('title', 'description', 'dueDate', 'priority2');
 let mewTodo = createTodo('title', 'description', 'dueDate', 'priority1');
 
+let add = document.querySelector('.add');
+add.addEventListener('click', function() {
+    addForm();
+    let cancel = document.querySelector('.cancel');
+    let normal = document.querySelector(".default2");
+    let form = document.querySelector('form');
+    cancel.addEventListener('click', function() {
+    normal.removeChild(form);
+    add.addEventListener('click', addForm, {once: true});
+})
+}, {once: true});
+
 addTodo(todoList, newTodo);
 addTodo(todoList, wewTodo);
 addTodo(todoList, mewTodo);
@@ -24,6 +36,5 @@ addTodo(todoList, mewTodo);
 console.log(todoList);
 
 render(todoList);
-addForm();
 
 
