01885260dbf1121708b64211937d57e1283b50d7
reed756 <68922323+reed756@users.noreply.github.com>
2021-05-13 22:03:33 +0100

    started adding attributes to form so can be linked to addTodo function

--- src/addform.js
+++ src/addform.js
@@ -6,21 +6,42 @@ const addForm = () => {
     let br3 = document.createElement('br');
     let br4 = document.createElement('br');
 
+
     let labelTitle = document.createElement('label');
     let inputTitle = document.createElement('input');
     labelTitle.textContent = "Title:";
+    inputTitle.setAttribute('data', 'title');
+    inputTitle.setAttribute('type', 'text');
 
     let labelDescription = document.createElement('label');
     let inputDescription = document.createElement('input');
     labelDescription.textContent = "Description:";
+    inputDescription.setAttribute('data', 'description');
+    inputDescription.setAttribute('type', 'text');
 
     let labelDueDate = document.createElement('label');
     let inputDueDate = document.createElement('input');
     labelDueDate.textContent = "Due Date:";
+    inputDueDate.setAttribute('data', 'duedate');
+    inputDueDate.setAttribute('type', 'date');
 
-    let labelPriority = document.createElement('label');
-    let inputPriority = document.createElement('input');
-    labelPriority.textContent = "Priority:";
+    let labelPriority1 = document.createElement('label');
+    let inputPriority1 = document.createElement('input');
+    labelPriority1.textContent = "Priority:";
+    inputPriority1.setAttribute('data', 'priority');
+    inputPriority1.setAttribute('type', 'radio');
+
+    let labelPriority2 = document.createElement('label');
+    let inputPriority2 = document.createElement('input');
+    labelPriority2.textContent = "Priority:";
+    inputPriority2.setAttribute('data', 'priority');
+    inputPriority2.setAttribute('type', 'radio');
+
+    let labelPriority3 = document.createElement('label');
+    let inputPriority3 = document.createElement('input');
+    labelPriority3.textContent = "Priority:";
+    inputPriority3.setAttribute('data', 'priority');
+    inputPriority3.setAttribute('type', 'radio');
 
     let button = document.createElement('button');
     let cancel = document.createElement('button');
@@ -39,8 +60,12 @@ const addForm = () => {
     form.appendChild(labelDueDate);
     form.appendChild(inputDueDate);
     form.appendChild(br3);
-    form.appendChild(labelPriority);
-    form.appendChild(inputPriority);
+    form.appendChild(labelPriority1);
+    form.appendChild(inputPriority1);
+    form.appendChild(labelPriority2);
+    form.appendChild(inputPriority2);
+    form.appendChild(labelPriority3);
+    form.appendChild(inputPriority3);
     form.appendChild(br4);
     form.appendChild(button);
     form.appendChild(cancel);
--- src/index.js
+++ src/index.js
@@ -10,31 +10,49 @@ import { addForm } from './addform.js';
 
 pageLoad();
 
-let totalList = [];
-let todoList = [];
-let anotherList = [];
-let newTodo = createTodo('title', 'description', 'dueDate', 'priority3');
-let wewTodo = createTodo('title', 'description', 'dueDate', 'priority2');
-let mewTodo = createTodo('title', 'description', 'dueDate', 'priority1');
+// let totalList = [];
+// let anotherList = [];
+// let newTodo = createTodo('title', 'description', 'dueDate', 'priority3');
+// let wewTodo = createTodo('title', 'description', 'dueDate', 'priority2');
+// let mewTodo = createTodo('title', 'description', 'dueDate', 'priority1');
 
+const todoList = [];
 let add = document.querySelector('.add');
+let isClicked = false;
+
+
 add.addEventListener('click', function() {
-    addForm();
-    let cancel = document.querySelector('.cancel');
-    let normal = document.querySelector(".default2");
-    let form = document.querySelector('form');
-    cancel.addEventListener('click', function() {
-    normal.removeChild(form);
-    add.addEventListener('click', addForm, {once: true});
-})
-}, {once: true});
+    if (isClicked === false) {
+        addForm();
+        isClicked = true;
+        let cancel = document.querySelector('.cancel');
+        let normal = document.querySelector(".default2");
+        let form = document.querySelector('form');
+        cancel.addEventListener('click', function() {
+        normal.removeChild(form);
+        isClicked = false;
+        })
+    }
+});
 
-addTodo(todoList, newTodo);
-addTodo(todoList, wewTodo);
-addTodo(todoList, mewTodo);
+// add.removeEventListener('click', addForm);
 
-console.log(todoList);
+// addTodo(todoList, newTodo);
+// addTodo(todoList, wewTodo);
+// addTodo(todoList, mewTodo);
+
+// console.log(todoList);
 
 render(todoList);
 
 
+// add.addEventListener('click', function() {
+//     addForm();
+//     add.removeEventListener('click', addForm);
+//     let cancel = document.querySelector('.cancel');
+//     let normal = document.querySelector(".default2");
+//     let form = document.querySelector('form');
+//     cancel.addEventListener('click', function() {
+//     normal.removeChild(form);
+// })
+// });
